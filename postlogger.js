0112b26459f8f43a4b64021d149ec9ebfe67b23a
Nicholas Rempel <nbrempel@gmail.com>
2013-07-08 23:57:13 -0700

    the stuff

--- postlogger.js
+++ postlogger.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var app = express();
 
+app.use(express.bodyParser());
+
 console.log('POST to me and I will log it for you.');
 
 app.post('/', function(req, res) {
-    console.log(req.body);
+    console.dir(req.body);
+    res.send('nom nom nom\n');
 });
 
-app.get(function() {
-    console.log('POST to me and I will log it for you.');
+app.get('/', function(req, res) {
+    res.send('Try POSTing.\n');
 });
+
+app.listen('3000');
